974ef2ce907420ae18721329c81d96a194b47cc7
SripadaFusteps <48852587+SripadaFusteps@users.noreply.github.com>
2019-06-20 14:45:09 +0530

    add test cases files

---
+++ javatest/sri.java
@@ -0,0 +1,9 @@
+import java.uitl.*;
+public class sri
+  {
+  public static void main(String a[])
+    {
+      System.out.println("hello Fusteps");
+      System.out.println(" The first test is successfull!!!!!!!!!!!!!");
+    }
+}
